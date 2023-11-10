f69f187d262dbfaa2f1cca93a8acf38024c037fa
davidrgfoss <40008571+davidrgfoss@users.noreply.github.com>
2023-11-10 13:58:19 +0100

    Update conexion.php

--- assets/php/conexion.php
+++ assets/php/conexion.php
@@ -8,7 +8,7 @@ $contrasena = 'QG0N2thg';
 $nombreBaseDeDatos = 'prueba';
 
 // Crear una conexión a MongoDB con autenticación
-$client = new MongoDB\Client("mongodb://$usuario:$contrasena@$host:$puerto/$nombreBaseDeDatos");
+$client = new MongoDB\Client("mongodb://$usuario:$contrasena@$host:$puerto/$nombreBaseDeDatos?authMechanism=SCRAM-SHA-256");
 
 // Seleccionar la base de datos
 $db = $client->selectDatabase($nombreBaseDeDatos);
