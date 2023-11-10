449948109fc1b33bd8b3312b7b78755e22e0261a
davidrgfoss <40008571+davidrgfoss@users.noreply.github.com>
2023-11-10 13:13:56 +0100

    Update conexion.php

--- assets/php/conexion.php
+++ assets/php/conexion.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../../vendor/autoload.php'; // Carga la biblioteca de MongoD
 $host = 'localhost';
 $puerto = '32773';
 $usuario = 'davidrg';
-$contrasena = '1';
+$contrasena = 'QG0N2thg';
 $nombreBaseDeDatos = 'prueba';
 
 // Crear una conexión a MongoDB con autenticación
