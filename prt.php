13e3c092f6992b27fb4d3c8fde883db97e90cd28
davidrgfoss <40008571+davidrgfoss@users.noreply.github.com>
2023-11-10 18:59:54 +0100

    Create prt.php

---
+++ prt.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+try {
+    $client = new MongoDB\Client("mongodb://davidrg:98I7VkyhpWbhqz@localhost:32773/prueba");
+    $db = $client->selectDatabase('prueba');
+    $collection = $db->selectCollection('usuarios');
+
+    // Realiza una consulta simple para probar la conexión
+    $document = $collection->findOne();
+    var_dump($document);
+} catch (MongoDB\Driver\Exception\Exception $e) {
+    echo 'Error al conectar a MongoDB: ', $e->getMessage();
+}
