9679881afc5a9144dbe6f79cde594a0fd7aaa431
davidrgfoss <40008571+davidrgfoss@users.noreply.github.com>
2023-11-11 01:25:28 +0100

    Update conexion.php

--- assets/php/conexion.php
+++ assets/php/conexion.php
@@ -2,13 +2,13 @@
 require __DIR__ . '/../../vendor/autoload.php'; // Carga la biblioteca de MongoDB
 
 $host = 'localhost';
-$puerto = '32773';
+$puerto = '32768';
 $usuario = 'davidrg';
 $contrasena = '98I7VkyhpWbhqz';
 $nombreBaseDeDatos = 'prueba';
 
 // Crear una conexión a MongoDB con autenticación
-$client = new MongoDB\Client("mongodb://$usuario:$contrasena@$host:$puerto/$nombreBaseDeDatos?authMechanism=SCRAM-SHA-256");
+$client = new MongoDB\Client("mongodb://$usuario:$contrasena@$host:$puerto/$nombreBaseDeDatos?directConnection=true&serverSelectionTimeoutMS=2000&authSource=admin");
 
 // Seleccionar la base de datos
 $db = $client->selectDatabase($nombreBaseDeDatos);
