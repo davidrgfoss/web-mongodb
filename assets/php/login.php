234f8b28d6779db27d26c7f78126dfef094d3636
davidrgfoss <40008571+davidrgfoss@users.noreply.github.com>
2024-04-24 12:35:24 +0200

    Update login.php

--- assets/php/login.php
+++ assets/php/login.php
@@ -1,5 +1,6 @@
 <?php
 require __DIR__ . '/../../vendor/autoload.php'; // Carga la biblioteca de MongoDB
+session_start(); // Mueve session_start() al principio para asegurarte de que las sesiones siempre se manejan primero
 
 // Conexión a la base de datos de usuarios
 $host = 'localhost';
@@ -11,18 +12,20 @@ $nombreBaseDeDatosUsuarios = 'prueba';
 $clientUsuarios = new MongoDB\Client("mongodb://$usuario:$contrasena@$host:$puerto/$nombreBaseDeDatosUsuarios?directConnection=true&serverSelectionTimeoutMS=2000&authSource=admin");
 $dbUsuarios = $clientUsuarios->selectDatabase($nombreBaseDeDatosUsuarios);
 
-session_start();
-if (!isset($_SESSION["user_id"]) || $_SESSION["user_id"] == null) {
+if (!isset($_SESSION["user_id"])) {
     echo "<script>alert('Necesitas estar registrado para acceder!');window.location='index.php';</script>";
     exit;
 }
 
 $collectionUsuarios = $dbUsuarios->usuarios;
 $datosUsuario = $collectionUsuarios->findOne(['_id' => new MongoDB\BSON\ObjectId($_SESSION["user_id"])]);
+if (!$datosUsuario) {
+    echo "<script>alert('Sesión no válida, por favor vuelve a iniciar sesión.');window.location='../formulario de acceso/index.php';</script>";
+    exit;
+}
 
-// Verificar si el usuario logueado es "raul"
-if (isset($datosUsuario['username']) && strtolower($datosUsuario['username']) === 'raul') {
-    // Conexión a la base de datos de productos
+// Verificar si el usuario logueado es "raul" y cargar información adicional si es necesario
+if (strtolower($datosUsuario['username']) === 'raul') {
     $nombreBaseDeDatosProductos = 'gnrec';
     $clientProductos = new MongoDB\Client("mongodb://$usuario:$contrasena@$host:$puerto/$nombreBaseDeDatosProductos?directConnection=true&serverSelectionTimeoutMS=2000&authSource=admin");
     $dbProductos = $clientProductos->selectDatabase($nombreBaseDeDatosProductos);
