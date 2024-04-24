76a7ac9ed421af1770ac03d7658c9d41b191921c
davidrgfoss <40008571+davidrgfoss@users.noreply.github.com>
2024-04-24 12:37:58 +0200

    Update login.php

--- assets/php/login.php
+++ assets/php/login.php
@@ -1,40 +1,30 @@
 <?php
-require __DIR__ . '/../../vendor/autoload.php'; // Carga la biblioteca de MongoDB
-session_start(); // Mueve session_start() al principio para asegurarte de que las sesiones siempre se manejan primero
+require __DIR__ . '/../../vendor/autoload.php'; // Asegura que la ruta al autoload es correcta
+session_start(); // Inicia sesión al principio del script
 
-// Conexión a la base de datos de usuarios
+// Parámetros de conexión
 $host = 'localhost';
 $puerto = '32769';
 $usuario = 'davidrg';
 $contrasena = '98I7VkyhpWbhqz';
 $nombreBaseDeDatosUsuarios = 'prueba';
 
+// Crear una conexión a MongoDB con autenticación
 $clientUsuarios = new MongoDB\Client("mongodb://$usuario:$contrasena@$host:$puerto/$nombreBaseDeDatosUsuarios?directConnection=true&serverSelectionTimeoutMS=2000&authSource=admin");
 $dbUsuarios = $clientUsuarios->selectDatabase($nombreBaseDeDatosUsuarios);
-
-if (!isset($_SESSION["user_id"])) {
-    echo "<script>alert('Necesitas estar registrado para acceder!');window.location='index.php';</script>";
-    exit;
-}
-
 $collectionUsuarios = $dbUsuarios->usuarios;
-$datosUsuario = $collectionUsuarios->findOne(['_id' => new MongoDB\BSON\ObjectId($_SESSION["user_id"])]);
-if (!$datosUsuario) {
-    echo "<script>alert('Sesión no válida, por favor vuelve a iniciar sesión.');window.location='../formulario de acceso/index.php';</script>";
-    exit;
-}
 
-// Verificar si el usuario logueado es "raul" y cargar información adicional si es necesario
-if (strtolower($datosUsuario['username']) === 'raul') {
-    $nombreBaseDeDatosProductos = 'gnrec';
-    $clientProductos = new MongoDB\Client("mongodb://$usuario:$contrasena@$host:$puerto/$nombreBaseDeDatosProductos?directConnection=true&serverSelectionTimeoutMS=2000&authSource=admin");
-    $dbProductos = $clientProductos->selectDatabase($nombreBaseDeDatosProductos);
+// Asumiendo que estos datos vienen de un formulario de login
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $usuario = $collectionUsuarios->findOne(['username' => $_POST['username']]);
 
-    try {
-        $collectionProductos = $dbProductos->productos;
-        $productos = $collectionProductos->find()->toArray();
-    } catch (Exception $e) {
-        echo "<p>Error al cargar la información de los productos: " . $e->getMessage() . "</p>";
+    if ($usuario && password_verify($_POST['password'], $usuario['password'])) {
+        $_SESSION['user_id'] = (string) $usuario['_id']; // Convierte ObjectId a string
+        header("Location: perfil.php");
+        exit;
+    } else {
+        echo "<script>alert('Usuario o contraseña incorrectos.'); window.location='../formulario de acceso/index.php';</script>";
+        exit;
     }
 }
 ?>
